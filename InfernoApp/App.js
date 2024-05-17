fdb10378f10e21729fb33e82c14741d130eb0df6
Akutagavaa <sofiia.khapal@nure.ua>
2024-05-17 15:28:11 +0200

    Update App.js

--- InfernoApp/App.js
+++ InfernoApp/App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import CustomButton from './components/CustomButton';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.buttonContainer}>
+                <CustomButton
+                    title="Sign up"
+                    onPress={() => alert('Button clicked!')}
+                />
+            </View>
+            <View style={styles.textContainer}>
+                <Text style={styles.text}>
+                    Якщо ви вже зареєстровані
+                    <TouchableOpacity onPress={() => alert('а теперь мы')}>
+                        <Text style={styles.linkText}> Увійдіть</Text>
+                    </TouchableOpacity>
+                </Text>
+            </View>
+        </SafeAreaView>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    buttonContainer: {
+        position: 'absolute',
+        bottom: '15%',
+        alignItems: 'center',
+    },
+    textContainer: {
+        position: 'absolute',
+        bottom: '25%',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 18,
+        textAlign: 'center',
+    },
+    linkText: {
+        color: '#FC6636',
+
+    },
 });
+
+export default App;
